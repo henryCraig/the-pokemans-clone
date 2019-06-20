20e97901bfb4c597cdd546421fc2ee64f66d605a
Jeff Bullock <thetanbull@gmail.com>
2019-06-20 13:29:01 -0400

    Renames table columns

--- pokemonClone/db/seeds.rb
+++ pokemonClone/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+pokemon = Pokemon.create([{ name: 'Bulbasaur', poketype: 'Grass'}, { name: 'Charmander', poketype: 'Fire'}])
+trainer = Trainer.create([{ name: 'Ash Ketchem'}])
